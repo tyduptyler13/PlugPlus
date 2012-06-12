ecf7d38e70406b8dd0b0ea69f2783c179c1d4e33
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-11 19:52:41 -0600

    Interface Update
    Corrected Spelling of resources
    removed unused resources
    incremented version

---
+++ Plug+/js/script.js
@@ -0,0 +1,7 @@
+// Auto* section
+Plugcontrols = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red' id='ppaj'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red'>AutoWoot</button></div>";
+$(document).ready(function(e) {
+    $('#dj-console').append(Plugcontrols);
+});
+
+
