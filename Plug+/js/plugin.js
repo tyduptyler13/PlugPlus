24cf5c29af68fb0f28f375048688205a29862dd5
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-11 21:12:37 -0600

    Added AutoJoin
    UNTESTED!!

--- Plug+/js/plugin.js
+++ Plug+/js/plugin.js
@@ -2,8 +2,10 @@ var s = document.createElement('script');
 s.src = chrome.extension.getURL("js/script.js");
 document.head.appendChild(s);
 
-$("body").append("<div style='display:none;' id=\"ppEvents\" hidden></div>");
+//The following isn't used yet.
+/*$("body").append("<div style='display:none;' id=\"ppEvents\" hidden></div>");
 
 $("#ppEvents").get(0).addEventListener("baseEvent",function(e_data){
 	chrome.extention.sendRequest({type:"event",data:e_data});
 });
+*/
--- Plug+/js/script.js
+++ Plug+/js/script.js
@@ -20,3 +20,14 @@ API.addEventListener(API.DJ_ADVANCE, function(dj){
 	if($('#ppaw').data('active')!='true') return; //AutoWoot Off
 	$('#button-vote-positive').click(); //Woot
 });
+
+API.addEventListener(API.DJ_UPDATE, function(){
+	if ($('#ppaj').data('active')!='true') return; //autoJoin off
+	if ($('#button-dj-quit').css('display')!="none") return;//Already a dj.
+	if ($('#button-dj-waitlist-leave').css('display')!="none") return; //Already on waitlist
+	if ($('#button-dj-play').css('display')!="none"){//Check for waitlist.
+		$('#button-dj-play').click();//Join
+	}else if ($('#button-dj-waitlist-join').css('display')!="none"){//Waitlist available
+		$('#button-dj-waitlist-join').click();//Join
+	}
+});
