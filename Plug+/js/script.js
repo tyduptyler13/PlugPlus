2c6d3c9c76427f9009caf39a2962e607fd36a7a9
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-11 21:44:41 -0600

    Completed for Release 1
    A few improvements like immediate results and explanations.

--- Plug+/js/script.js
+++ Plug+/js/script.js
@@ -1,28 +1,38 @@
 // Auto* section
-PlugControls = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red' id='ppaj'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red'>AutoWoot</button></div>";
+PlugControls = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red' id='ppaj' title='AutoJoin will place you back in line after your turn is over.'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red' title='This will autoWoot any song people play so you get more points.'>AutoWoot</button></div>";
 $(document).ready(function(e) {
     $('#dj-console').append(PlugControls);
 });
 
 $('#plugPlus button').bind('click',function(eventData){
-	console.log(eventData);
 	var pressed = eventData.srcElement;
 	if($(pressed).data('active')!='true'){
 		$(pressed).data('active','true').css('background-color','green');
+		if (pressed.id == "ppaj"){
+			autoJoin();
+		}
+		if (pressed.id == "ppaw"){
+			autoWoot();
+		}
 	}else{
 		$(pressed).data('active','false').css('background-color','red');
 	}
 });
 
-API.addEventListener(API.DJ_ADVANCE, function(dj){
+API.addEventListener(API.DJ_ADVANCE, autoWoot());
+
+API.addEventListener(API.DJ_UPDATE, autoJoin());
+
+function autoWoot(){
+	var dj = API.getDJs()[0];
 	if(dj == null) return; //no dj
 	if(dj == API.getSelf()) return; //don't woot yourself
 	if($('#ppaw').data('active')!='true') return; //AutoWoot Off
 	$('#button-vote-positive').click(); //Woot
-});
+}
 
-API.addEventListener(API.DJ_UPDATE, function(){
-	if ($('#ppaj').data('active')!='true') return; //autoJoin off
+function autoJoin(){
+		if ($('#ppaj').data('active')!='true') return; //autoJoin off
 	if ($('#button-dj-quit').css('display')!="none") return;//Already a dj.
 	if ($('#button-dj-waitlist-leave').css('display')!="none") return; //Already on waitlist
 	if ($('#button-dj-play').css('display')!="none"){//Check for waitlist.
@@ -30,4 +40,4 @@ API.addEventListener(API.DJ_UPDATE, function(){
 	}else if ($('#button-dj-waitlist-join').css('display')!="none"){//Waitlist available
 		$('#button-dj-waitlist-join').click();//Join
 	}
-});
+}
