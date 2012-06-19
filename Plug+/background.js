de7daedff5362dcbe1ddb62225203d44ebf3a09c
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-18 22:29:21 -0600

    Started work on background.js

--- Plug+/background.js
+++ Plug+/background.js
@@ -8,5 +8,26 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
   
-  
-  
+function notify(img, title, text){
+	var notification = webkitNotifications.createNotification(img,title,text);
+	notification.show();
+}
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponce) {
+	if (request.action=="notify"){
+		notify(request.img,request.title,request.text);
+	}else if (request.action=="getSave"){
+		sendResponce({value: localStorage[request.data]});
+	}
+});
+
+if (localStorage["showNotifications"]=="true") {//One time check every time background starts.
+	if (localStorage["lastVersion"] !== undefined) {
+		if (localStorage["lastVersion"] !== chrome.app.getDetails().version) {
+			nofify('icon.png',"Update","Plug+ has been updated!");
+			localStorage["lastVersion"] = chrome.app.getDetails().version;
+		} else {
+			localStorage["lastVersion"] = chrome.app.getDetails().version;
+		}
+	}
+}
