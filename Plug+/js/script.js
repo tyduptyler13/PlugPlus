1e8248c7f02397bf6ecc40a83a6c794ab02be721
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-18 21:45:08 -0600

    Tracking and some bug fixes

--- Plug+/js/script.js
+++ Plug+/js/script.js
@@ -1,32 +1,49 @@
+  //Tracking
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32685589-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 // Auto* section
 PlugControls = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red; border-radius:5px;' id='ppaj' title='Join automatically whenever your turn ends.'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red;border-radius:5px' title='Woot every song that plays.'>AutoWoot</button></div>";
 $(document).ready(function(e) {
+	if (document.location.pathname=="/") return;//Don't add to front page
+	
     $('#dj-console').append(PlugControls);
-});
+	
+	API.addEventListener(API.DJ_ADVANCE, function(){
+		ppDJAdvance();
+		autoWoot();
+	});
 
-$('#plugPlus button').bind('click',function(eventData){
-	var pressed = eventData.srcElement;
-	if($(pressed).data('active')!='true'){
-		$(pressed).data('active','true').css('background-color','green');
-		if (pressed.id == "ppaj"){
-			autoJoin();
+	API.addEventListener(API.DJ_UPDATE, function(){
+		autoJoin();
+	});
+	
+	$('#plugPlus button').bind('click',function(eventData){
+		var pressed = eventData.srcElement;
+		if($(pressed).data('active')!='true'){
+			$(pressed).data('active','true').css('background-color','green');
+			if (pressed.id == "ppaj"){
+				autoJoin();
+			}
+			if (pressed.id == "ppaw"){
+				autoWoot();
+			}
+		}else{
+			$(pressed).data('active','false').css('background-color','red');
 		}
-		if (pressed.id == "ppaw"){
-			autoWoot();
-		}
-	}else{
-		$(pressed).data('active','false').css('background-color','red');
-	}
+	});
 });
 
-API.addEventListener(API.DJ_ADVANCE, function(){
-	ppDJAdvance();
-	autoWoot();
-});
 
-API.addEventListener(API.DJ_UPDATE, function(){
-	autoJoin();
-});
+
+
 
 function autoWoot(){
 	var dj = API.getDJs()[0];
