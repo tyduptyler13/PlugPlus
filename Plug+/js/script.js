c5cba765045d45a61a6ca1ae20e2d94c305a9d69
Tyler Scott <tyduptyler13@yahoo.com>
2012-06-18 21:24:09 -0600

    Updated style of buttons to look a little better

--- Plug+/js/script.js
+++ Plug+/js/script.js
@@ -1,5 +1,5 @@
 // Auto* section
-PlugControls = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red' id='ppaj' title='Join automatically whenever your turn ends.'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red' title='Woot every song that plays.'>AutoWoot</button></div>";
+PlugControls = "<div id='plugPlus' style='position:absolute;bottom:0px;width:310px;color:black;margin:5px;'><button style='background-color:red; border-radius:5px;' id='ppaj' title='Join automatically whenever your turn ends.'>AutoJoin</button><button id='ppaw' style='float:right;background-color:red;border-radius:5px' title='Woot every song that plays.'>AutoWoot</button></div>";
 $(document).ready(function(e) {
     $('#dj-console').append(PlugControls);
 });
